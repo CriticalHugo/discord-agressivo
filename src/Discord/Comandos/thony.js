9de9b7a169ab5575104ef55f3c4624f988977f37
CriticalHugo <hugomissionario@gmail.com>
2022-02-20 14:29:30 -0300

    Thony com mais xingamentos

--- src/Discord/Comandos/thony.js
+++ src/Discord/Comandos/thony.js
@@ -2,6 +2,7 @@ module.exports = {
     name: "thony",
     aliases: ["<@!844999176319926294>"],
     exe(message, args){
-        message.channel.send(`<@!${message.author.id}> menor, não fala esse nome na minha frente mais não`);
+        const xingamento = [`<@!${message.author.id}> menor, não fala esse nome na minha frente mais não`, "Turbina", "Cachoeira"];
+        message.channel.send(`${xingamento[Math.floor(Math.random()*xingamento.length)]}`);
     }
 }
