7c99de974e373a7cefa0990de29937f65b6d1810
CriticalHugo <hugomissionario@gmail.com>
2022-02-19 23:12:33 -0300

    dando a vida ao bot agressivo

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Client, Intents } = require('discord.js');
-const TOKEN='OTQ0NzQxMjI1MDk1OTIxNjc1.YhGA1A.yyfYCs7Yx9ZbP7X0UJ5dnF-yKbI';
+const TOKEN='OTQ0NzQxMjI1MDk1OTIxNjc1.YhGA1A.eB7CfuFWiABxLWAXlEU19_-8TeM';
 
 const myIntents = new Intents();
 myIntents.add(Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES);
