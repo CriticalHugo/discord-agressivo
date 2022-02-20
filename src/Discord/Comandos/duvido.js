ace88940080798157d8c5dc1af0c5cd5335b12b4
CriticalHugo <hugomissionario@gmail.com>
2022-02-20 17:05:06 -0300

    Novas opções de xingamentos

---
+++ src/Discord/Comandos/duvido.js
@@ -0,0 +1,8 @@
+const xingamento = ["Meu pau no seu ouvido"];
+
+module.exports = {
+    name: "duvido",
+    exe(message, args) {
+        message.channel.send(`${xingamento[Math.floor(Math.random()*xingamento.length)]}`);
+    }
+}
