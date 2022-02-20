3bc896d7d1222857be749a1e00b081146a3cf506
CriticalHugo <hugomissionario@gmail.com>
2022-02-20 18:47:08 -0300

    Novas opções de xingamentos

--- src/Discord/Comandos/que-brisa.js
+++ src/Discord/Comandos/que-brisa.js
@@ -4,6 +4,7 @@ const xingamento = [
 
 module.exports = {
     name: "que brisa",
+    aliases: ["q brisa"],
     exe(message, args) {
         message.channel.send(`${xingamento[Math.floor(Math.random()*xingamento.length)]}`);
     }
--- src/Discord/Comandos/que-calor.js
+++ src/Discord/Comandos/que-calor.js
@@ -5,6 +5,7 @@ const xingamento = [
 
 module.exports = {
     name: "que calor",
+    aliases: ["q calor"],
     exe(message, args) {
         message.channel.send(`${xingamento[Math.floor(Math.random()*xingamento.length)]}`);
     }
